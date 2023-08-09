7d1a169d7437ed310f1f52bc73e0297aab8da304
Sara Anita <sara.anita.desa@gmail.com>
2023-08-09 20:56:30 -0300

    fixed: iniciando js e ajustando html e css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+import dados from './data/ghibli/ghibli.js'
+
+const cardContainer = document.querySelector(".card")
+const descricao = document.querySelector(".cardDescricao")
+
+
+ 
+cardContainer.innerHTML = ghibli
+descricao.innerHTML = ghibli
+// console.log(dados.films[0].title) como chamar uma propriedade especifica.
+
+const ghibli = dados.films;
+
+dados.map( ghibli => `
+      <div class="card">
+      <img class="cardImg"  scr=${ghibli[0].poster}>
+      </div>
+      
+    `
+)
+
+//    <div class="cardDescricao">
+//       <p><strong>Título:</strong>${dados.films[0].title}</p>
+//       console.log(`dados,films[0].title`)
+//       <p><strong>Ano:</strong>${dados.films[0].release_date}</p>
+//       </div>
